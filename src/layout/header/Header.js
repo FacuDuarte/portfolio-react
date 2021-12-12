a212c118217204dcf6456aae11ae3d545fa88a8e
Facundo <cailloureloaded@outlook.com>
2021-12-12 19:26:38 -0300

    primer commmit

---
+++ src/layout/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css"
+import fotoCV from '../../assets/fotoPersonal.png'
+
+const Header = () => {
+    return (
+        <div className='bgHeader'>
+            <div className='contentHeader'>
+                <img src={fotoCV} alt='foto sobre mí' className='personalImg'/>
+                <div className='textHeader'>
+                    <p className='text'>Hi!</p>
+                    <p className='textName'>Soy Facundo Duarte</p>
+                    <p className='text'>Front End Developer</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
